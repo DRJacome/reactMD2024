ca3e1df14b312b9fc572d778b39c42221e06f180
David Rodríguez <jacodaro@hotmail.com>
2024-06-22 23:17:20 +0200

    06 Creación de proyecto; completados Mostrar lista de productos JSON, añadir filtros por categoría y precio

---
+++ projects/06-shopping-cart/README.md
@@ -0,0 +1,17 @@
+# Enunciado.
+
+1. Crea una tienda online.
+
+* [X] Muestra una lista de productos que vienen de un JSON.
+* [X] Añade un filtro por categoría.
+* [X] Añade un filtro por precio.
+
+Haz uso de useContext para evitar pasar props innecesarias.
+
+2. Cienda online.
+
+1. [ ] Haz que se puedan añadir los productos a un carrito.
+2. [ ] Haz que se puedan eliminar los productos de carrito.
+3. [ ] Haz que se puedan modiciar la cantidad de productos del carrito.
+4. [ ] Sincroniza los cambios del carrito con la lista de productos.
+5. [ ] Guarda en un localStorage el carrito para que se recupere al recargar la página.
---
+++ projects/06-shopping-cart/src/App.jsx
@@ -0,0 +1,38 @@
+/* INSTALAR:
+- A nivel de repositorio como dependencia de desarrollo (-D):
+npm i standard -D
+*/
+import { products as initialProducts } from "./mocks/products.json";
+import { Products } from "./components/Products.jsx";
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+
+function App() {
+    const [products] = useState(initialProducts);
+    const [filters, setFilters] = useState({
+        category: "all",
+        minPrice: 0,
+    });
+
+    const filterProducts = (products) => {
+        return products.filter((product) => {
+            return (
+                product.price >= filters.minPrice &&
+                (filters.category === "all" ||
+                    product.category === filters.category)
+            );
+        });
+    };
+
+    const filteredProducts = filterProducts(products);
+
+    return (
+        <>
+            <Header changeFilters={setFilters}/>
+            <Products products={filteredProducts} />
+        </>
+    );
+}
+
+export default App;
